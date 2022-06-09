e196a757ec4f11281c1b16613ed2509c27d5909f
Wenpineda <cp0428032021@unab.edu.sv>
2022-06-08 22:02:04 -0600

    ApiWeb

---
+++ WebAPI-WLCP/WebAPI-WLCP/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+
+
+namespace WebAPI_WLCP.Controllers 
+{
+    public class ProductoController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        // GET: Producto
+        public IHttpActionResult Add()
+        {
+            using (Models.BarDbEntities db = new Models.BarDbEntities())
+            {
+
+                var oProducto = new Models.Producto();
+                oProducto.nomProd = "Jugo Valle"; 
+                oProducto.descripcion = "Sabor Limon"; 
+                oProducto.precio =Convert.ToDecimal(0.50); 
+                oProducto.cantidad = 5; 
+                oProducto.estado = 2;
+                db.Producto.Add(oProducto);    
+                db.SaveChanges();
+            }
+            return Ok("Hecho");
+        }
+    }
+}
